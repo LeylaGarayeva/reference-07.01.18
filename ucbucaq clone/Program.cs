04c97bc878ce4c49e0135144f681681f95814de6
Leyla Garayeva <leylagarayeva1@gmail.com>
2018-01-07 19:22:57 +0400

    first commit

---
+++ ucbucaq clone/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ucbucaq_clone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ucbucaq uc = new ucbucaq();
+            uc.a = 15;
+            uc.b = 15;
+            uc.c = 20;
+
+            ucbucaq uc2 = uc.copy();
+
+
+            int yari = uc.perimetr() / 2;
+            uc2.a = yari + uc.a;
+            uc2.b = yari + uc.b;
+            uc2.c = yari + uc.c;
+
+            Console.WriteLine(uc.perimetr());
+            Console.WriteLine(uc2.perimetr());
+            Console.ReadLine();
+
+        }
+    }
+
+    class ucbucaq {
+
+        public int a;
+        public int b;
+        public int c;
+
+        public int perimetr()
+        {
+            return a + b + c;
+        }
+
+        public ucbucaq copy()
+        {
+            ucbucaq temp = new ucbucaq();
+            temp.a = a;
+            temp.b = b;
+            temp.c = c;
+            return temp;
+        }
+    }
+
+}
---
+++ ucbucaq clone/reference_out.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ucbucaq_clone
+{
+    class reference_out
+    {
+        static void Mainn()
+        {
+
+            double s = 0;
+            int r = 15;
+            calcarea(r,out s);
+
+            Console.WriteLine(s);
+            Console.ReadLine();
+
+        }
+
+        static void calcarea(int r,out double s)
+        {
+            s = Math.PI * r * r;
+        }
+
+     
+
+
+
+    }
+}
+
+
